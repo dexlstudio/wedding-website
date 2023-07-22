219dceb686cae37220364ae577f69405adc8e22d
Egor Larionov <egor.larionov@gmail.com>
2023-07-22 11:48:46 -0700

    Updates to fix build

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby"
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Dian & Egor`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://dexl.ca`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
